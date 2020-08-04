e26cf6e45e48bf7ed279776216839337271f9578
Jeremy <lavigne.jeremy@post.com>
2020-08-04 17:01:10 +0200

    part 1c

--- src/index.js
+++ src/index.js
@@ -1,52 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
 
+const Header = ({course}) => {
   return (
-    <>
-      <h1>{props.course}</h1>
-    </>
+      <h1>{course}</h1>
   )
 }
 
-const Part = (props) => {
-
+const Part = ({part}) => {
   return (
-    <>
       <p>
-        {props.part.name} {props.part.exercises}
+        {part.name} {part.exercises}
       </p>
-    </>
   )
 }
 
-const Content = (props) => {
-
+const Content = ({parts}) => {
   return (
     <>
-      <Part 
-        part={props.parts[0]}
-      />
-      <Part 
-        part={props.parts[1]}
-      />
-      <Part 
-        part={props.parts[2]}
-      />
-
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </>
   ) 
 }
 
-const Total = (props) => {
-  
+const Total = ({parts}) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
-    <p>Number of exercises 
-      {props.parts[0].exercises 
-      + props.parts[1].exercises 
-      + props.parts[2].exercises}
-    </p>
+    <p> Number of exercises {total} </p>
   )
 }
 
@@ -71,19 +54,11 @@ const App = () => {
 
   return (
     <div>
-
-      <Header 
-        course={course.name} 
-      />
+      <Header course={course.name} />
       
-      <Content 
-        parts={course.parts}
-      />
+      <Content parts={course.parts} />
 
-      <Total 
-        parts={course.parts}
-      />
-
+      <Total parts={course.parts} />
     </div>
   )
 }
